751c15876883b5e8833e5bd4255a955c9b3b80fb
samman-varshney <sammanvarshney345@gmail.com>
2025-08-28 13:13:53 +0530

    Add frontend and backend as normal folders

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+const { createWorkflow } = require('./controllers/workflowController');
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'your request is accepted' })
+})
+
+app.post('/api/query', async (req, res) => {
+    const { prompt } = req.body;
+    const result = await geminiModel.generateContent(prompt);
+    const message = result.response.candidates[0].content.parts[0].text;
+    res.json({ message: message });
+})
+
+app.post('/api/generate/workflow', createWorkflow);
+
+app.listen(3000, () => {
+    console.log('serving at http://localhost:3000');
+})      
