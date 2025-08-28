bea02d171d999572419d9919bc5c62daeb19b087
Samman Varshney <sammanvarshney345@gmail.com>
2025-08-28 14:41:10 +0530

    changes committed in frontend

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 const { createWorkflow } = require('./controllers/workflowController');
 
 app.use(cors({
-  origin: 'https://codeflow-one.vercel.app/',
+  origin: 'https://codeflow-one.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
